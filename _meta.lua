6ac328f71fd8f1da971c1214c8c79c71628f77a8
wtb <aagknaag@hotmail.com>
2026-06-07 19:34:59 +0200

    Flatten directory structure
    Move plugin files to the repo root so it can be used as a contrib submodule. Adjust release workflow to repack under wireguard.koplugin/. Add OWNER. Bump to 1.0.2.

--- _meta.lua
+++ _meta.lua
@@ -2,6 +2,6 @@ return {
     name        = "wireguard",
     fullname    = "WireGuard VPN",
     description = [[Connect to a WireGuard VPN from KOReader.]],
-    version     = "1.0.1",
+    version     = "1.0.2",
     author      = "Wouter ten Brinke",
 }
